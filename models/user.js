083e762795e17095f4b5c910dcc11c91655eca61
milind7agarwal <155805902+milind7agarwal@users.noreply.github.com>
2026-02-15 22:26:08 +0530

    Enhance user schema with validation and constraints

--- models/user.js
+++ models/user.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username : String,
-    password : String 
+    username : {
+        type: String,
+        required:true,
+        trim:true,
+        lowercase: true,
+        unique: true,
+        minlength: [3, 'Usernme must not exceed more than 3 chracter'] 
+    },
+    password : {
+        type: String,
+        required:true,
+        trim:true,
+        unique: true,
+        minlength: [5, 'password must not exceed more than 5 chracter'] 
+    } 
 })
 
 const userModel = mongoose.model('user', userSchema)
